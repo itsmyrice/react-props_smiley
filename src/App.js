9ede9cdfabe95239073fc555fbf6567fc84f005f
Glory ann Conwi <gaconwi@googlemail.com>
2023-11-27 16:31:00 +0100

    challenge, 3rd task, done.

--- src/App.js
+++ src/App.js
@@ -2,5 +2,9 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Smiley isHappy={true} />;
+}
+
+function Smiley ({ isHappy }) {
+if (isHappy) {return <h1>Happy me</h1>} else {return <h1>Sad me</h1>}
 }
